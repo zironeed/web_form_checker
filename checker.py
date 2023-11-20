cfd083b783540f572eec1c7aaaf00e78e11d2e76
Adil S <forever@nzironeed.ru>
2023-11-20 21:01:58 +0500

    fix handling of incorrect keys

--- checker.py
+++ checker.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 db = TinyDB('forms_db.json')
 
 
-@app.route('/', methods=['GET'])
-def main_page():
-    return 'Hello'
-
-
 @app.route('/get_form')
 def get_form():
     data = request.args.to_dict()
@@ -23,9 +18,8 @@ def get_form():
         template_len = len(template_fields.values())
         result = 0
 
-        for key in template_fields.keys():
-            if key not in validated_data.keys():
-                continue
+        if template_fields.keys() != validated_data.keys():
+            continue
 
         for form_type in form_types:
             if form_type in validated_data.values():
